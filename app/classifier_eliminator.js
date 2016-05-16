c4d04bfc846c1114a1e4db2f63d08d1b26ce8d4f
Tarik Akyol <akyoltarik@gmail.com>
2016-05-16 19:01:07 +0200

    Pushing my bits

--- app/classifier_eliminator.js
+++ app/classifier_eliminator.js
@@ -1,53 +1,98 @@
 const CHARS = 'abcdefghijklmnopqrstuvwxyz\''
-var MAP = []
-var DIMENSION = 2
+const VOWELS = 'aeiou'
+const CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
 
-/*
-* thins to add
-* 1. starting letters probability
-* 2. ending letters probability
-* 3. Wovel ratio
-* */
+const DATA_MAPPING = ['S2','S3','E2','E3','G2','G3'] // E=>End G=>Global N=>Letter Count
+const DATA_SEPARATOR = 124 // ascii code of |
+const MAP = DATA_MAPPING.map(() => [])
 
-module.exports = {
+const MAX_WORD_LENGTH = 13
+const MIN_VOWEL_RATIO = 0.09
+const MAX_VOWEL_RATIO = 0.71
 
-  init: data => {
-    data.forEach((b,i) => {
+const setMap = data => {
+  let counter = 0
+  let separatorCounter = 0
+  let numberOfLetters = 0
 
-      if (b.toString() == '|'.charCodeAt(0))
-        DIMENSION++
+  data.forEach((b,i) => {
 
-      if (i % DIMENSION == 0) {
+    if (b.toString() == DATA_SEPARATOR) {
+      separatorCounter++
+      counter = 0
+
+    } else {
+      numberOfLetters = DATA_MAPPING[separatorCounter][1]
+
+      if (counter % numberOfLetters == 0) {
         let block = ''
-        for (let j = 0; j < DIMENSION; j++)
+
+        for (let j = 0; j < numberOfLetters; j++)
           block += CHARS[data[i + j]]
 
-        MAP.push(block)
+        MAP[separatorCounter].push(block)
       }
-    })
-  },
+      counter++
+    }
+  })
+}
 
-  test: word => {
-    let isEnglish = true
+const checkLength = word => {
+  return word.length < MAX_WORD_LENGTH
+}
 
-    if(word.length >= 15)
-      return false
+const checkData = word => {
+  let flag = true
 
-    for (var d = 2; d < DIMENSION; d++) {
-      if (isEnglish) {
-        for (let i = 0; i < word.length - (d - 1); i++) {
-          if (isEnglish) {
-            let block = ''
+  DATA_MAPPING.forEach((data, index) => {
 
-            for (let j = 0; j < d; j++)
-              block += word[i + j]
+    if(!flag)
+      return
 
-            if (MAP.indexOf(block) != -1)
-              isEnglish = false
-          }
-        }
+    let typeOfData = data[0]
+    let numberOfLetters = data[1]
+
+    if(typeOfData == 'S') {
+      let block = word.substr(0, numberOfLetters)
+
+      if(MAP[index].indexOf(block) != -1)
+        flag = false
+
+    } else if(typeOfData == 'E') {
+      let block = word.substr(-numberOfLetters)
+
+      if(MAP[index].indexOf(block) != -1)
+        flag = false
+
+    } else if (typeOfData  == 'G') {
+      for (let i = 0; i < word.length - (numberOfLetters - 1); i++) {
+        let block = ''
+
+        for (let j = 0; j < numberOfLetters; j++)
+          block += word[i + j]
+
+        if (MAP[index].indexOf(block) != -1)
+          flag = false
       }
     }
-    return isEnglish
-  }
+  })
+
+  return flag
+}
+
+const checkRatio = word => {
+  let ratio
+  let noOfVowels = word.match(new RegExp(`[${VOWELS}]`, 'gi'))
+  let noOfConsonants = word.match(new RegExp(`[${CONSONANTS}]`, 'gi'))
+
+  noOfVowels = noOfVowels ? noOfVowels.length : 0
+  noOfConsonants = noOfConsonants ? noOfConsonants.length : 0
+  ratio = noOfVowels / (noOfVowels + noOfConsonants)
+
+  return ratio > MIN_VOWEL_RATIO && ratio < MAX_VOWEL_RATIO
+}
+
+module.exports = {
+  init: data => setMap(data),
+  test: word => checkLength(word) && checkData(word) && checkRatio(word)
 }
--- app/test.js
+++ app/test.js
@@ -4,8 +4,15 @@ const http = require('follow-redirects').http
 const classifier = require('./classifier_eliminator')
 const combinedStream = require('combined-stream');
 
-const srcs = ['../resources/sample_elimination_2d.io',
-              '../resources/sample_elimination_3d.io']
+const srcs = [
+  '../resources/sample_start_elimination_2d.io',
+  '../resources/sample_start_elimination_3d.io',
+  '../resources/sample_ending_elimination_2d.io',
+  '../resources/sample_ending_elimination_3d.io',
+  '../resources/sample_elimination_2d.io',
+  '../resources/sample_elimination_3d.io'
+]
+
 const dest = '../resources/sample_elimination_all.io'
 const zip = '../resources/sample_elimination_all.io.gz'
 
@@ -55,14 +62,14 @@ function test() {
       let words = JSON.parse(body)
       let correctCounter = 0
       for (var word in words) {
-        if(words.hasOwnProperty(word)) {
+        if (words.hasOwnProperty(word)) {
           let isEnglish = classifier.test(word)
           isEnglish == words[word] && correctCounter++
           // console.log(word, words[word], isEnglish)
         }
       }
 
-      sum = sum + (100 * correctCounter) / Object.keys(words).length
+      sum = sum + ((100 * correctCounter) / Object.keys(words).length)
 
       process.stdout.write(`Counter: ${counter} | Average: ${(sum/counter).toFixed(2)}%\r`);
       // console.log('Result', (100 * correctCounter) / Object.keys(words).length, '%')
